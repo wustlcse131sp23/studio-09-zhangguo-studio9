7456d9bbe5a8d0b186cb3d769ce94b6c680d4f35
zhangzirui <zhangzirui@localhost>
2023-04-13 15:39:59 -0500

    //

--- src/studio9/UniversityDatabase.java
+++ src/studio9/UniversityDatabase.java
@@ -1,26 +1,43 @@
 package studio9;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import assignment7.Student;
 
 public class UniversityDatabase {
 	//TODO: Complete this class according to the studio instructions
+	private final Map<String,Student>students;
+	public UniversityDatabase() {
+		this.students=new HashMap<>();
+	}
+	
 
 	public void addStudent(String accountName, Student student) {
-		// TODO
+		students.put(accountName, student);
 	}
 
 	public int getStudentCount() {
-		// TODO
-		return 0;
+		return students.size();
 	}
 
 	public String lookupFullName(String accountName) {
-		// TODO: Complete according to studio instructions
-		return null;
+		Student a=students.get(accountName);
+		if(a==null){
+			return null;
+		}else {
+			return a.getFullName();
+		}
+		
 	}
 
 	public double getTotalBearBucks() {
-		// TODO
-		return 0.0;
+		double sum=0;
+		for(String an : students.keySet()) {
+			Student a=students.get(an);
+			sum += a.getBearBucksBalance();
+		}
+		return sum;
+		
 	}
 }
